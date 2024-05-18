b552f74edbafe8cdfde61e0df7fc79694aa239a5
Edan Weinberg <edanweinberg@mail.tau.ac.il>
2024-05-18 12:14:10 +0300

    minor skeleton add

--- symnmf.py
+++ symnmf.py
@@ -1,1 +1,36 @@
-print("h")
+# imports
+# import c module named symnmf
+import sys
+
+# default values - max iter, eps?
+
+def main():
+    argv = sys.argv
+    ## no need to validate, assumption 2
+    k = int(argv[1])
+    goal = argv[2]
+    path = argv[3]
+
+
+    # switch case for goal
+    match goal: # TODO replace with actual functions
+        case "symnmf":
+            print("symnmf") # c api with symnmf()
+        case "sym":
+            print("sym")
+        case "ddg":
+            print("ddg")
+        case "norm":
+            print("norm")
+        case _:
+            throwOfficialError()
+
+def throwOfficialError():
+    print("An Error Has Occurred")
+    sys.exit() #TODO how to terminate properly??
+
+
+
+
+if __name__ == "__main__":
+    main()
